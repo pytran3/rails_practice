8315ff88827903c60efbc6f12297e6d269fd3520
pytran3 <34180469+pytran3@users.noreply.github.com>
2020-12-07 19:36:30 +0900

    ハロワ

--- build_container.sh
+++ build_container.sh
@@ -1,2 +1,2 @@
 docker build -t rails_vanilla .
-docker run -d -v `pwd`/myapp:/home/myapp --name rails_practice rails_vanilla tail -f /dev/null
+docker run -d -v `pwd`/myapp:/home/myapp -p 3000:3000 --name rails_practice rails_vanilla tail -f /dev/null
