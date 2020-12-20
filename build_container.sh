cbe9187381f39a5a5b9c4cb2c58fd7373a9b3b29
pytran3 <34180469+pytran3@users.noreply.github.com>
2020-12-20 22:12:23 +0900

    make docker run safe

--- build_container.sh
+++ build_container.sh
@@ -1,2 +1,2 @@
-docker build -t rails_vanilla .
-docker run -d -v `pwd`:/home/myapp -p 3000:3000 --name rails_practice rails_vanilla tail -f /dev/null
+docker build -t rails_vanilla . &&
+    docker run -d -v `pwd`:/home/myapp -p 3000:3000 --name rails_practice rails_vanilla tail -f /dev/null
