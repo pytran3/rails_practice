6ae6602ca2c43c0a8d9f0f7c3b842ae0dfe5b78a
pytran3 <34180469+pytran3@users.noreply.github.com>
2020-12-22 10:12:36 +0900

    update build_container.sh

--- build_container.sh
+++ build_container.sh
@@ -1,2 +1,3 @@
-docker build -t rails_vanilla . &&
-    docker run -d -v `pwd`:/home/myapp -p 3000:3000 --name rails_practice rails_vanilla tail -f /dev/null
+#!/bin/bash -eu
+docker build -t rails_vanilla .
+docker run -d -v `pwd`:/home/myapp -p 3000:3000 --name rails_practice rails_vanilla tail -f /dev/null
