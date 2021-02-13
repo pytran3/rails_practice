f7970a16e23c6fafc78781d4f38f39d8c9f8b8aa
pytran3 <34180469+pytran3@users.noreply.github.com>
2021-02-13 20:19:17 +0900

    add authorization

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     if (user_id = session[:user_id])
