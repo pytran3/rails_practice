4d72e0adcf72d06bfc10279b2b64ce972d266f2b
pytran3 <34180469+pytran3@users.noreply.github.com>
2021-03-24 02:22:17 +0900

    Implement delete micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,10 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-
+  
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   private
     # メールアドレスをすべて小文字にする
     def downcase_email
